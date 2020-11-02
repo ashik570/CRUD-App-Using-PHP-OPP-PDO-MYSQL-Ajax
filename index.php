19d7fdc118e75ae9332dd479c4ce04c35bdbe2a3
ashik570 <ashikplus570@gmail.com>
2020-11-02 19:49:56 +0600

    CDN Included

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 	<title>Crud App Using PHP-OPP, PDO-MYSQL & AJAX</title>
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/dt-1.10.22/datatables.min.css"/>
 	<link rel="stylesheet" href="">
 </head>
 <body>
@@ -18,5 +19,8 @@
 
 	<!-- Latest compiled JavaScript -->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+	<script src="https://kit.fontawesome.com/a076d05399.js"></script>
+	<script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.10.22/datatables.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 </body>
 </html>
